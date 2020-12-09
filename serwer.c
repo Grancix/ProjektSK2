9a24af42c025509262aa9c54ef1d19a5384eef9c
Grancix <75678075+Grancix@users.noreply.github.com>
2020-12-09 11:05:57 +0100

    Add files via upload

--- serwer.c
+++ serwer.c
@@ -71,7 +71,7 @@ int main(int argc, char * argv[])
 
 	shared_data = (char **) shmat(shmid, (void **)0, 0);
 
-	if(shared_data == (char *)-1)
+	if(shared_data == (char **)-1)
 	{
 		printf(" blad shmat!\n");
 		exit(1);
